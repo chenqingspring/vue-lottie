2370f417b7685fd0a23e9c82f13f7cdf06389e3d
Martin Lissmats <martin.lissmats@kognity.com>
2019-07-12 13:31:36 +0200

    allow loop option to be any value

--- src/lottie.vue
+++ src/lottie.vue
@@ -30,7 +30,7 @@
       this.anim = lottie.loadAnimation({
           container: this.$refs.lavContainer,
           renderer: 'svg',
-          loop: this.options.loop !== false,
+          loop: this.options.loop,
           autoplay: this.options.autoplay !== false,
           animationData: this.options.animationData,
           rendererSettings: this.options.rendererSettings
